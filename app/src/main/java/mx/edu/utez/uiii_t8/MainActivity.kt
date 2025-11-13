85ea635c7c1c282f1ffc43861f4a8ed643b85245
val1-boop <20243ds003@utez.edu.mx>
2025-11-13 14:30:31 -0600

    proyecto de Aplicación Multimedia con Compose, Room y MVVM

---
+++ app/src/main/java/mx/edu/utez/uiii_t8/MainActivity.kt
@@ -0,0 +1,47 @@
+package mx.edu.utez.uiii_t8
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import mx.edu.utez.uiii_t8.ui.theme.UIII_T8Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            UIII_T8Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    UIII_T8Theme {
+        Greeting("Android")
+    }
+}
